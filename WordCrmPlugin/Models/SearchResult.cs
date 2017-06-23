ef9033a574156e23faf45699aef9abc54255951d
Thomas Heigl <thnew2@gmail.com>
2017-06-23 13:23:19 +0200

    Added SearchResult Model

---
+++ WordCrmPlugin/Models/SearchResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace WordCrmPlugin.Models
+{
+    class SearchResult
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public string ShortDescription
+        {
+            get
+            {
+                if (Content.Length > 150)
+                {
+                    return Content.Substring(0, 150);
+                }
+
+                return Content;
+            }
+        }
+
+        public string ContentToPaste
+        {
+            get
+            {
+                return $"{Content}\n\nSee {Link} for more details.";
+            }
+        }
+
+        public Uri Link { get; set; }
+    }
+}
