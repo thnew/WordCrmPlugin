1d49885c1c862bad918fe5a1aa98d507a655d713
Thomas Heigl <thnew2@gmail.com>
2017-06-23 08:32:18 +0200

    Initial

---
+++ WordCrmPlugin/KnowledgeBaseSearch.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Interop.Word;
+
+namespace WordCrmPlugin
+{
+    public partial class KnowledgeBaseSearch : UserControl
+    {
+        private Document _document;
+        private ListViewItem SelectedItem {
+            get
+            {
+                foreach(ListViewItem item in listView1.Items)
+                {
+                    if (item.Selected)
+                    {
+                        return item;
+                    }
+                }
+
+                return null;
+            }
+        }
+
+        public KnowledgeBaseSearch(Document doc)
+        {
+            _document = doc;
+
+            InitializeComponent();
+
+            var item = new ListViewItem("Thema A");
+            var item2 = new ListViewItem("Thema B");
+
+            listView1.Items.Add(item);
+            listView1.Items.Add(item2);
+        }
+        
+        private void ListView1_DoubleClick(object sender, EventArgs e)
+        {
+            var item = SelectedItem;
+            if(item == null)
+            {
+                return;
+            }
+
+            _document.Paragraphs[1].Range.InsertParagraphBefore();
+            _document.Paragraphs[1].Range.Text = item.Text;
+        }
+    }
+}
---
+++ WordCrmPlugin/ThisAddIn.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+using Microsoft.Office.Tools.Word;
+using Microsoft.Office.Tools;
+
+namespace WordCrmPlugin
+{
+    public partial class ThisAddIn
+    {
+        //private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        //{
+        //}
+
+        //private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        //{
+        //}
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            Application.DocumentOpen += new Word.ApplicationEvents4_DocumentOpenEventHandler((Word.Document doc) => {
+                KnowledgeBaseSearch ctrl = new KnowledgeBaseSearch(doc);
+                string title = "Knowledgebase Articles";
+                CustomTaskPane ctp = Globals.ThisAddIn.CustomTaskPanes.Add(ctrl, title);
+                ctp.Visible = true;
+                ctp.Width = 220;
+                ctp.DockPosition = Microsoft.Office.Core.MsoCTPDockPosition.msoCTPDockPositionRight;
+            });
+
+            //Startup += new EventHandler(ThisAddIn_Startup);
+            //Shutdown += new EventHandler(ThisAddIn_Shutdown);
+            Application.DocumentBeforeSave += new Word.ApplicationEvents4_DocumentBeforeSaveEventHandler((Word.Document Doc, ref bool SaveAsUI, ref bool Cancel) =>
+            {
+                Doc.Paragraphs[1].Range.InsertParagraphBefore();
+                Doc.Paragraphs[1].Range.Text = "Thomas.";
+
+            });
+        }
+
+        #endregion
+    }
+}
